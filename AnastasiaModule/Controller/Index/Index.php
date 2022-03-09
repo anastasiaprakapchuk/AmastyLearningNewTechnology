7c5bf24d4b50b0fbfc100c461a1e724da35a94cc
anastasiaprakapchuk <anastasiacalej@gmail.com>
2022-03-09 21:43:13 +0300

    changes

--- AnastasiaModule/Controller/Index/Index.php
+++ AnastasiaModule/Controller/Index/Index.php
@@ -5,11 +5,12 @@ namespace Amasty\AnastasiaModule\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action implements HttpGetActionInterface
 {
     public function execute()
     {
-        echo "Привет Magento. Привет Amasty. Я готова тебя покорить!";
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
