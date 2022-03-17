e9c97ac94e796b8625e5990beac11cc8a8b3d0e6
anastasiaprakapchuk <anastasiacalej@gmail.com>
2022-03-17 19:12:24 +0300

    a few changes

--- AnastasiaModule/Controller/Cart/Add.php
+++ AnastasiaModule/Controller/Cart/Add.php
@@ -7,6 +7,7 @@ use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\CatalogInventory\Model\Stock\StockItemRepository;
 
 
 class Add extends Action
@@ -26,17 +27,24 @@ class Add extends Action
      */
     protected $resultRedirectFactory;
 
+    /**
+     * @var StockItemRepository
+     */
+    protected $stockItemRepository;
+
 
     public function __construct(
         Context                    $context,
         CheckoutSession            $session,
         ProductRepositoryInterface $productRepository,
-        RedirectFactory            $resultRedirectFactory
+        RedirectFactory            $resultRedirectFactory,
+        StockItemRepository $stockItemRepository
     )
     {
         $this->session = $session;
         $this->productRepository = $productRepository;
         $this->resultRedirectFactory = $resultRedirectFactory;
+        $this->stockItemRepository=$stockItemRepository;
         parent::__construct($context);
     }
 
@@ -47,22 +55,41 @@ class Add extends Action
         $sku = $data['sku'];
         $qty = $data['qty'];
 
-        //добавляем товар в корзину
+        //получаем продукт по sku
+        try{
+            $product = $this->productRepository->get($sku);
+            $productId=$product->getId();
+            $productStock = $this->stockItemRepository->get($productId);;
+            $maxProducts=$productStock->getQty();
 
-        $product = $this->productRepository->get($sku);
 
+        //проверяем некоторые условия
 
+        if($product->getTypeId()!=='simple'){
+            $this->messageManager->addWarningMessage('The product must be simple!');
+        }
 
-        $quote = $this->session->getQuote();
-        if (!$quote->getId()) {
+        else if($maxProducts<$qty){
+            $this->messageManager->addWarningMessage("You can order a maximum of $maxProducts products!");
+        }
+
+        else{
+            //сохраняем
+            $quote = $this->session->getQuote();
+            if (!$quote->getId()) {
+                $quote->save();
+            }
+            $quote->addProduct($product, $qty);
             $quote->save();
         }
-        $quote->addProduct($product, $qty);
-        $quote->save();
-
-
-        $resultRedirect = $this->resultRedirectFactory->create();
-        return $resultRedirect->setPath('anastasia');
+        }
+        catch (Exeption $ex){
+            $this->messageManager->addWarningMessage('This product does not exist!');
+        } finally {
+            //редирект на страницу с формой
+            $resultRedirect = $this->resultRedirectFactory->create();
+            return $resultRedirect->setPath('anastasia');
+        }
 
     }
 }
